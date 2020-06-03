78beb7aec25c4730981be549a64b9d04098a9163
Carla <carligir@gmail.com>
2020-06-03 00:30:01 -0300

    Guia 1

--- js/apis.js
+++ js/apis.js
@@ -1,13 +1,17 @@
-var mapa // Mapa que vamos a modificar
+var mapa; // Mapa que vamos a modificar
+var posicionCentral = {
+  lat: -31.599722,
+  lng: -64.5530555
+}; // Coordenadas de la Estación Astrofísica Bosque Alegre
 
 /* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
 
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
-    /* Modificá la variable mapa con el constructor Map().
-    Tendrás que asignarle un valor de zoom y
-    un centro igual a la variable posicionCentral. */
-
+  /* Modificá la variable mapa con el constructor Map().
+  Tendrás que asignarle un valor de zoom y
+  un centro igual a la variable posicionCentral. */
+  mapa = new google.maps.Map(document.getElementById('map'), {center: posicionCentral, zoom: 14});
   geocodificadorModulo.inicializar()
   marcadorModulo.inicializar()
   direccionesModulo.inicializar()
